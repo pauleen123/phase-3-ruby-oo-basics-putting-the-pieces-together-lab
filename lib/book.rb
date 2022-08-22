3683c3510fec0182610b229af0c1d96aad672d9a
+ <+>
2022-08-22 11:07:54 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
 
+class Book
+
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+
+    def initialize (title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end 
+
+# title = Book.new("And Then There Were None")
+# author = Book.new("Agatha Christie")
+
+
+
+
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = "Nike"
+    end
+
+    def cobble 
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end 
+end 
+
+
